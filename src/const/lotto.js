b367001e3409fc24ed420780b40893c131563ca1
Junepil Lee <hanu9257@gmail.com>
2025-11-02 17:18:40 +0800

    chore: move constants to seperate directory

--- src/const/lotto.js
+++ src/const/lotto.js
@@ -3,6 +3,13 @@ const LOTTO = Object.freeze({
   LENGTH: 6,
   MAX: 45,
   MIN: 1,
+  PRIZE: {
+    1: 2000000000,
+    2: 30000000,
+    3: 1500000,
+    4: 50000,
+    5: 5000,
+  },
 });
 
 export { LOTTO };
