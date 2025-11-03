d80a19260fa49a75e8f7bbbea396a985a27a5524
Junepil Lee <hanu9257@gmail.com>
2025-11-03 20:29:17 +0800

    feat: add barrel file for model

---
+++ src/model/index.js
@@ -0,0 +1,5 @@
+import Lotto from './Lotto.js';
+import LottoStatistician from './LottoStatistician.js';
+import LottoVender from './LottoVender.js';
+
+export { Lotto, LottoStatistician, LottoVender };
