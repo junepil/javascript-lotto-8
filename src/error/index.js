cc34dd5b7e1b6040263877a215023d20b7b2b829
Junepil Lee <hanu9257@gmail.com>
2025-11-03 20:30:30 +0800

    chore: rename path using `.js` in error barrel file

--- src/error/index.js
+++ src/error/index.js
@@ -1,7 +1,7 @@
 import CustomError from './CustomError.js';
 import LottoNumberError from './LottoNumberError.js';
 import LottoVenderError from './LottoVenderError.js';
-import LottoStatisticianError from './LottoStatisticianError';
+import LottoStatisticianError from './LottoStatisticianError.js';
 
 export {
   CustomError,
